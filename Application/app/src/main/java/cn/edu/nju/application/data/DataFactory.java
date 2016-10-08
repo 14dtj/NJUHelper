a3c3148133c3da92a4085a0cecf579f08904863d
tj Du <457391179@qq.com>
2016-10-08 21:55:53 +0800

    同步

---
+++ Application/app/src/main/java/cn/edu/nju/application/data/DataFactory.java
@@ -0,0 +1,24 @@
+package cn.edu.nju.application.data;
+
+/**
+ * Created by tjDu on 2016/10/8.
+ */
+
+public class DataFactory {
+    private static IUserDao userDao;
+    private static IPostDao postDao;
+
+    public static IUserDao getUserDao() {
+        if (userDao != null) {
+            userDao = new UserDaoImpl();
+        }
+        return userDao;
+    }
+
+    public static IPostDao getPostDao() {
+        if (postDao != null) {
+            postDao = new PostDaoImpl();
+        }
+        return postDao;
+    }
+}
--- Application/app/src/main/java/cn/edu/nju/application/presentation/presenter/HomeFragmentPresenter.java
+++ Application/app/src/main/java/cn/edu/nju/application/presentation/presenter/HomeFragmentPresenter.java
@@ -1,7 +1,15 @@
 package cn.edu.nju.application.presentation.presenter;
 
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cn.edu.nju.application.data.DataFactory;
 import cn.edu.nju.application.data.IPostDao;
-import cn.edu.nju.application.data.PostDaoImpl;
+import cn.edu.nju.application.presentation.fragment.HomeFragment;
+import cn.edu.nju.application.presentation.model.Post;
 
 /**
  * Created by tjDu on 2016/10/8.
@@ -9,8 +17,23 @@ import cn.edu.nju.application.data.PostDaoImpl;
 
 public class HomeFragmentPresenter {
     private IPostDao dao;
+    private HomeFragment view;
 
-    public HomeFragmentPresenter() {
-      
+    public HomeFragmentPresenter(HomeFragment view) {
+        dao = DataFactory.getPostDao();
+        this.view = view;
+    }
+
+    public void loadPosts() {
+        List<Post> list = dao.showPostList("旅行");
+
+        ArrayList<HashMap<String, String>> data = null;
+        for (Post post : list) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("title", post.getTitle());
+            map.put("content", post.getContent());
+            data.add(map);
+        }
+        view.setPostAdapter(data);
     }
 }
